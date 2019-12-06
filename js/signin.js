1fe1c91031ce398fc5221f99e53d1cbda2bfc402
manOwO <maian1611@gmail.com>
2019-12-06 15:26:40 +0700

    ahihi061219

--- js/signin.js
+++ js/signin.js
@@ -1,23 +1,3 @@
-// toggle between hiding and showing the dropdown content 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
-
-
 var btnsignin = document.getElementById("btn-signIn");
 var username = document.getElementById("username-or-email");
 var password = document.getElementById("password");
@@ -33,7 +13,7 @@ async function clksignin() {
 
     let uservalid = false
     let userid = ''
-    let userNameEmail = '' 
+    let userNameEmail = ''
     // get data
     await db.collection('user').get().then((snapshot) => {
         // console.log(snapshot);
@@ -49,7 +29,6 @@ async function clksignin() {
                 userNameEmail = doc.data().username;
                 console.log(doc.id);
             }
-
         });
         if (uservalid == true) {
             console.log("Login successfully");
@@ -57,47 +36,13 @@ async function clksignin() {
             console.log(password.value);
             localStorage.setItem('id', userid);
             localStorage.setItem('username', userNameEmail);
-
-            var ifyousignin = document.getElementById("ifyousignin");
-            ifyousignin.style.display = "flex";
-            var ifyounotsignin = document.getElementById("ifyounotsignin");
-            ifyounotsignin.style.display = "none";
-
         }
         else if (uservalid == false) {
             alert("This username/email has not been used for register *Or* wrong password.");
-            // var alert = document.getElementById("alert");
-            // alert.style.display = "flex";
         }
 
         });
 }
-
-// if ("username"&&"id" in localStorage) {
-//     var ifyousignin = document.getElementById("ifyousignin");
-//     ifyousignin.style.display = "flex";
-// }else {
-//     var ifyounotsignin = document.getElementById("ifyounotsignin");
-//     ifyounotsignin.style.display = "none";
-// }
-
-// if (localStorage.getItem("username") == null) {
-//     var ifyounotsignin = document.getElementById("ifyounotsignin");
-//     ifyounotsignin.style.display = "none";
-// }
-// else {
-//     var ifyousignin = document.getElementById("ifyousignin");
-//     ifyousignin.style.display = "flex"; 
-// }
-
-// if(typeof(localStorage.getItem('username'))==="null") {
-//     var ifyounotsignin = document.getElementById("ifyounotsignin");
-//     ifyounotsignin.style.display = "none";
-// }
-// else {
-//     var ifyousignin = document.getElementById("ifyousignin");
-//     ifyousignin.style.display = "flex";
-// }
 
 // // có nút logout r :) sau khi đã login n có cái value trong local storage n bla bla bla
 // var hello = document.getElementById("text-SignIn");
@@ -110,3 +55,22 @@ async function clksignin() {
 //     localStorage.clear();
 //     hello.textContent = "Sign in";
 // }
+var ifyounotsignin = document.getElementById("ifyounotsignin");
+var ifyousignin = document.getElementById("ifyousignin");
+setInterval (function() {
+    if (localStorage.getItem('id') === null){
+    ifyousignin.style.display = "none";
+    ifyounotsignin.style.display = "flex";
+    console.log("failed to get id");
+}
+    else {
+    ifyounotsignin.style.display = "none";
+    ifyousignin.style.display = "flex";
+}}, 100);
+
+var log_out = document.getElementById("log-out");
+log_out.addEventListener('click', logout);
+function logout() {
+    localStorage.clear();
+    location.reload();
+}
