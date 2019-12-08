a13013bb5ae09fbd1ce9681e6782cd2624bb1531
Your Name <wslinh.nguyen.60073@wellspring.edu.vn>
2019-12-08 23:17:52 +0700

    0812

--- js/debateroom_apformat.js
+++ js/debateroom_apformat.js
@@ -16,3 +16,83 @@ function myFunction() {
         }
       }
     } 
+
+
+var dropBt = document.getElementById('dropBt');
+dropBt.addEventListener('click', function() {
+  var select = document.getElementById('select');
+  // select.classList += "showhaha";
+  select.style.display = 'block';
+})
+
+var text = document.getElementById('text');
+var matchupOp = document.getElementById('matchup-op');
+var matchup = document.getElementById('matchup');
+var chatboxOp = document.getElementById('chatbox-op');
+var chatbox = document.getElementById('chatbox');
+
+matchupOp.addEventListener("click",function(){
+  matchup.style.display = 'flex';
+  chatbox.style.display = 'none';
+  select.style.display = 'none';
+  text.textContent = "Matchup information";
+})
+
+chatboxOp.addEventListener('click', function(){
+  chatbox.style.display = 'flex';
+  matchup.style.display = 'none';
+  select.style.display = 'none';
+  text.textContent = "Chatbox";
+})
+
+var input = document.getElementById("input");
+var sendBt = document.getElementById("send");
+sendBt.addEventListener('click', sendFunction);
+
+function sendFunction() {
+  inputVal = input.value;
+  if (inputVal != '') {
+    var newM = document.createElement('div');
+    var newUserava = document.createElement('div');
+    var newUsermess = document.createElement('div');
+    var newUsername = document.createElement('div');
+    var newMessage = document.createElement('div');
+  
+    newM.className = 'm';
+    newUserava.className = 'user-ava';
+    newUsermess.className = 'user-mess';
+    newUsername.className = 'username';
+    newMessage.className = 'message';
+  
+    mContainer.appendChild(newM);
+    newM.appendChild(newUserava);
+    newM.appendChild(newUsermess);
+    newUsermess.appendChild(newUsername);
+    newUsermess.appendChild(newMessage);
+    // console.log(newM);
+  
+    newUsername.textContent = "Username";
+    newMessage.innerHTML = inputVal;
+
+    input.value = '';
+  }
+}
+
+
+var sumHeight = 0
+var m = document.getElementsByClassName("m");
+for (i=0;i<m.length; i+=1) {
+  mHeight = m[i].offsetHeight;
+  sumHeight += mHeight
+};
+var mContainer = document.getElementById("m-container");
+var mContainerHeight = mContainer.offsetHeight+15;
+if (sumHeight>mContainerHeight) {
+  mContainer.style.overflowY = "scroll";
+}
+
+
+
+
+
+
